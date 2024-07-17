d08ad5d656dade39f464e326acc5941a3f3bb499
famojuro <adeniyifamojuro@gmail.com>
2024-07-17 22:45:36 +0100

    kotlin demo with spring boot

---
+++ src/main/kotlin/com/famotech/spring_boot_kotlin_demo/SpringBootKotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.famotech.spring_boot_kotlin_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/famotech/spring_boot_kotlin_demo/SpringBootKotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.famotech.spring_boot_kotlin_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
